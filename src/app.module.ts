072846f4d8926d0845a4c8cdb2f54b1e345e5503
kuchuk borom debbarma <kuchukboromdebbarma@outlook.com>
2024-12-05 15:10:43 +0530

    Removed unnecessary files and added config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from './common/common.module';
 import * as process from 'node:process';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UserModule,
+    CommonModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DATABASE_URL,
-      username: '',
-      password: '',
-      database: 'anonymask',
-
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
     }),
-    CommonModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    console.log(JSON.stringify(process.env));
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
 }
+
+const dummy = {
+  type: 'postgres',
+  host: process.env.DATABASE_URL,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+};
+
+console.log(JSON.stringify(dummy));
 bootstrap();
