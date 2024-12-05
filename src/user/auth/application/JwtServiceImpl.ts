032c947db17ba7da931172c4ef6d18d6b8f79495
-foxT <fokazmchakma4427@gmail.com>
2024-12-05 16:37:41 +0530

    Added methods to generate and verify jwt (testing pending)

--- src/user/auth/application/JwtServiceImpl.ts
+++ src/user/auth/application/JwtServiceImpl.ts
@@ -1,15 +1,27 @@
 import { Injectable } from "@nestjs/common";
+import { JwtService as NestJwtService } from '@nestjs/jwt';
 import JwtService from "../api/service/JwtService";
 
 @Injectable()
 export default class JwtServiceImpl implements JwtService{
-
+  constructor(private readonly jwtService: NestJwtService) {}
 
   generateJwt(claims: Map<string, string>, subject: string): string {
-    throw new Error("Method not implemented.");
+    const claimsObject = Object.fromEntries(claims);
+    const payload = { ...claimsObject, sub: subject };
+
+    return this.jwtService.sign(payload);
   }
   verifySignature(token: string) {
-    throw new Error("Method not implemented.");
+    try {
+      const decoded = this.jwtService.verify(token);
+      return decoded;
+    } catch (error) {
+      return {
+        error: 'Invalid token',
+        details: error.message
+      };
+    }
   }
 
 }
