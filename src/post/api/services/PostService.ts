ff6eb042c891de069d837f3f0ebb2e353687a13f
-foxT <fokazmchakma4427@gmail.com>
2024-12-12 02:52:04 +0530

    Add Post module, service, and exceptions (implementation pending)
    - Created PostModule with necessary configurations(added the database stuff and exported PostService).
    - Added PostService with abstract methods to handle post functionality.
    - Implemented PostNotFoundException as a custom error for missing posts.
    - Set up placeholders for the implementation in PostServiceImpl.

--- src/post/api/services/PostService.ts
+++ src/post/api/services/PostService.ts
@@ -1,5 +1,41 @@
-export default abstract class PostService {
-  abstract createPost(): void;
-  abstract deletePost(postID: string): void;
-  abstract updatePost(postID: string): void;
+import PostDTO from '../dto/PostDTO';
+
+export abstract class PostService {
+  /**
+   * Create a new post
+   * @param postData The data required to create a new post, including title and content
+   * @return The created PostDTO object containing the post details
+   */
+  abstract createPost(postData: {
+    title: string;
+    content: string;
+  }): Promise<PostDTO>;
+
+  /**
+   * Get a post by its ID
+   * @param postId The ID of the post to retrieve
+   * @return A PostDTO object representing the post, or null if the post was not found
+   */
+  abstract getPostById(postId: string): Promise<PostDTO | null>;
+
+  /**
+   * Update an existing post
+   * @param postId The ID of the post to be updated
+   * @param postData The data used to update the post, including optional title and required content
+   * @return void
+   */
+  abstract updatePost(
+    postId: string,
+    postData: {
+      title?: string;
+      content: string;
+    },
+  ): Promise<void>;
+
+  /**
+   * Delete a post by its ID
+   * @param postId The ID of the post to delete
+   * @return void
+   */
+  abstract deletePost(postId: string): Promise<void>;
 }
