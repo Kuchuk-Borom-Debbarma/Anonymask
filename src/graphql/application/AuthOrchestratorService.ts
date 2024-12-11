997d76f2753d547772dcb65872b05db4d66735d4
-foxT <fokazmchakma4427@gmail.com>
2024-12-12 01:13:49 +0530

    removed UserFiledDTO

--- src/graphql/application/AuthOrchestratorService.ts
+++ src/graphql/application/AuthOrchestratorService.ts
@@ -1,9 +1,9 @@
-import { Injectable } from "@nestjs/common";
-import JwtService from "../../user/auth/api/service/JwtService";
-import AuthService from "../../user/auth/api/service/AuthService";
-import UserService from "../../user/api/service/UserService";
-import { OAuthProvider } from "../../user/auth/api/Provider";
-import { UserInfoDTO } from "../../user/auth/api/dto/UserInfoDTO";
+import { Injectable } from '@nestjs/common';
+import JwtService from '../../user/auth/api/service/JwtService';
+import AuthService from '../../user/auth/api/service/AuthService';
+import UserService from '../../user/api/service/UserService';
+import { OAuthProvider } from '../../user/auth/api/Provider';
+import { UserInfoDTO } from '../../user/auth/api/dto/UserInfoDTO';
 
 @Injectable()
 export class AuthOrchestrator {
@@ -11,19 +11,15 @@ export class AuthOrchestrator {
     private readonly jwtService: JwtService,
     private readonly authService: AuthService,
     private readonly userService: UserService,
-  ) { }
+  ) {}
 
-  public async generateAccessToken(
-    code: string,
-    oauthProvide: OAuthProvider
-  ) {
-    const userInfo: UserInfoDTO = await this.authService.getUserInfoFromOAuthToken(code);
+  public async generateAccessToken(code: string, oauthProvide: OAuthProvider) {
+    const userInfo: UserInfoDTO =
+      await this.authService.getUserInfoFromOAuthToken(code);
     try {
       await this.userService.createBaseUser(userInfo.id, userInfo.name);
-    }
-    catch (error) { }
+    } catch (error) {}
     const token = this.jwtService.generateJwt(null, userInfo.id);
     return token;
   }
-
 }
